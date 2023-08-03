f76e69fc2f0b74b0b5ac7e25f0ecb7258f3b5f6d
Star-Leeee <139939885+Star-Leeee@users.noreply.github.com>
2023-08-04 07:30:56 +0800

    Add files via upload

---
+++ project 18/18.py
@@ -0,0 +1,8 @@
+#send a tx on Bitcoin testnet...
+import requests
+import re
+url='https://en.bitcoin.it/wiki/Testnet'
+tx={'tx':'111222333'}
+post_html=requests.post(url,data=tx)
+#输出并解析返回的响应信息
+print(post_html.text)
