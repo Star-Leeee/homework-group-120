00d9c1fdd0790b189d4fc92b41e758645e895306
Star-Leeee <139939885+Star-Leeee@users.noreply.github.com>
2023-08-04 17:55:39 +0800

    Update and rename 18.py to BitCoin.py

--- project 18/BitCoin.py
+++ project 18/BitCoin.py
@@ -1,8 +1,7 @@
 #send a tx on Bitcoin testnet...
 import requests
-
 url='https://en.bitcoin.it/wiki/Testnet'
-tx={'tx':'111222333'}
+tx={'tx':'114514'}
 post_html=requests.post(url,data=tx)
 #输出并解析返回的响应信息
 print(post_html.text)
